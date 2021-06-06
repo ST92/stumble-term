8d0f58e2aca09ff2555c9e7d49c9c373aa10a42a
ST92 <marsdprogrammer@gmail.com>
2021-06-06 21:11:38 +0200

    add local machine cache info to output

--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,122 @@
-use std::io;
-use std::time::Instant;
 
-use std::sync::mpsc::channel;
-use std::thread;
+use std::io;                  
+use std::time::Instant;       
+                              
+use std::sync::mpsc::channel; 
+use std::thread;              
 
-use glutin::{
+use cache_size;
+                              
+use glutin::{                 
     event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-    ContextBuilder,
-};
-
+    event_loop::{ControlFlow, EventLoop},  
+    window::WindowBuilder,                 
+    ContextBuilder,                        
+};                                         
+                                           
 use femtovg::{renderer::OpenGl, Align, Baseline, Canvas, Color, Paint, Path, Renderer};
-
-struct StartupSettings {
+                                           
+struct StartupSettings {                   
     physical_size: glutin::dpi::PhysicalSize<i32>,
-    title: String,
-    default_font: String,
-
-}
-
-
-impl Default for StartupSettings {
-    fn default() -> Self {
-        Self {
+    title: String,                         
+    default_font: String,                          
+}                                                  
+                                                   
+impl Default for StartupSettings {                 
+    fn default() -> Self {                         
+        Self {                                     
             physical_size: glutin::dpi::PhysicalSize::new(1000, 670),
             title: String::from("Background task info"),
             /// a fixed font is used everywhere for simplicity
             default_font: String::from("/home/rne/Pobrane/cataclysmdda-0.D/data/font/fixedsys.ttf"),
-        }
-    }
-}
+        }                                          
+    }                                              
+}                                                  
+                                                   
+fn main() {                                        
+    let mut settings = StartupSettings::default(); 
 
-fn main() {
-    let mut settings = StartupSettings::default();
+    
+    let l1size = cache_size::l1_cache_size().unwrap_or(0);
+    let l2size = cache_size::l2_cache_size().unwrap_or(0);
+    let l3size = cache_size::l3_cache_size().unwrap_or(0);
 
-    let el = EventLoop::new();
+    let cache_description = format!("# L1: {}, L2: {}, L3: {}", l1size, l2size, l3size);
 
-    let wb = WindowBuilder::new()
+    settings.title += &cache_description[..];
+                                                   
+    let el = EventLoop::new();                     
+                                                   
+    let wb = WindowBuilder::new()                  
         .with_inner_size(settings.physical_size)
-        .with_resizable(false)
-        .with_title(settings.title);
-
+        .with_resizable(false)                  
+        .with_title(settings.title);            
+                                                
     let windowed_context = ContextBuilder::new().build_windowed(wb, &el).unwrap();
     let windowed_context = unsafe { windowed_context.make_current().unwrap() };
-
+                                                
     let renderer = OpenGl::new(|s| windowed_context.get_proc_address(s) as *const _)
-        .expect("Cannot create renderer");
+        .expect("Cannot create renderer");      
     let mut canvas = Canvas::new(renderer).expect("Cannot create canvas");
-    canvas.set_size(
-        settings.physical_size.width as u32,
-        settings.physical_size.height as u32,
+    canvas.set_size(                            
+        settings.physical_size.width as u32,    
+        settings.physical_size.height as u32,   
         windowed_context.window().scale_factor() as f32,
-    );
-    canvas
+    );                         
+    canvas                     
         .add_font(settings.default_font)
         .expect("Cannot add font");
-
+                               
     let start = Instant::now();
-    let mut prevt = start;
-
+    let mut prevt = start; 
+                           
     let mut perf = PerfGraph::new();
     let mut perf_shown = false;
-
+                                      
     let (sender, reciever) = channel();
-
+                                      
     let mut input_text = String::new();
     let mut line_starts: Vec<usize> = vec![0];
-
-    thread::spawn(move || {
-        let input = io::stdin();
-        loop {
+                                      
+    thread::spawn(move || {           
+        let input = io::stdin();      
+        loop {                        
             let mut buffer = String::new();
             match input.read_line(&mut buffer) {
                 Ok(_n) => sender.send(buffer).unwrap_or(()),
-                Err(_) => break,
-            }
-        }
-    });
-
-    el.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Poll;
-
-        match event {
-            Event::LoopDestroyed => return,
+                Err(_) => break,      
+            }                         
+        }                                    
+    });                                      
+                                             
+    el.run(move |event, _, control_flow| {   
+        *control_flow = ControlFlow::Poll;   
+                                             
+        match event {                        
+            Event::LoopDestroyed => return,  
             Event::WindowEvent { ref event, .. } => match event {
                 WindowEvent::Resized(physical_size) => {
                     windowed_context.resize(*physical_size);
-                }
+                }                            
                 WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                WindowEvent::KeyboardInput {
-                    input:
-                        KeyboardInput {
+                WindowEvent::KeyboardInput { 
+                    input:                   
+                        KeyboardInput {      
                             state: ElementState::Pressed,
                             virtual_keycode: Some(VirtualKeyCode::Q),
-                            ..
-                        },
-                    ..
+                            ..               
+                        },                   
+                    ..                       
                 } => *control_flow = ControlFlow::Exit,
-
-                WindowEvent::KeyboardInput {
-                    input:
-                        KeyboardInput {
+                                             
+                WindowEvent::KeyboardInput { 
+                    input:                   
+                        KeyboardInput {      
                             state: ElementState::Pressed,
                             virtual_keycode: Some(VirtualKeyCode::F3),
-                            ..
-                        },
-                    ..
+                            ..               
+                        },                   
+                    ..                       
                 } => perf_shown = !perf_shown,
                 _ => {}
             },
